9e45c3984fbde58bebb473db8eb7d3636bfa14ae
Raja Saffi Ullah <55308067+SaffiUllah1999@users.noreply.github.com>
2023-05-10 02:43:05 -0700

    Some Update

--- server/Socket_Screen.tsx
+++ server/Socket_Screen.tsx
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import P2P from "socket.io-p2p";
 const Socket_Url = "http://172.16.10.58:3000";
-const socket = io(Socket_Url);
+const socket = io(Socket_Url); 
 
 import React from "react";
 import { initialUpdaterRun } from "react-native-reanimated/lib/types/lib/reanimated2/animation";
